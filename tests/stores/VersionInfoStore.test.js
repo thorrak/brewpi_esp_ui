564fcdc036436cc683f63bf8130403465e553a2a
John <thorrak@users.noreply.github.com>
2023-07-21 19:58:54 -0400

    Convert test to use fixture file

--- tests/stores/VersionInfoStore.test.js
+++ tests/stores/VersionInfoStore.test.js
@@ -1,21 +1,14 @@
-import { createTestingPinia } from '@pinia/testing';
 import { setActivePinia, createPinia } from 'pinia'
 import { useVersionInfoStore } from '@/stores/VersionInfoStore.js';
 import { mande } from 'mande';
 import { jest } from '@jest/globals';
-
-// jest.mock('mande', () => {
-//     return {
-//         mande: jest.fn(),
-//     };
-// });
+import fs from 'fs';
+import path from 'path';
 
 jest.mock('mande');
 
 describe('VersionInfoStore', () => {
     beforeEach(() => {
-        // creates a fresh pinia and make it active so it's automatically picked
-        // up by any useStore() call without having to pass it to it
         setActivePinia(createPinia());
     });
 
@@ -23,13 +16,10 @@ describe('VersionInfoStore', () => {
         const store = useVersionInfoStore();
         expect(store.hasVersionInfo).toBe(false);
         expect(store.versionInfoError).toBe(false);
-        // add similar checks for other state properties
     });
 
     it('clears the version info correctly', async () => {
         const store = useVersionInfoStore();
-        // setting some state properties
-        expect(store.hasVersionInfo).toBe(false);
         store.hasVersionInfo = true;
         store.brewpiVersion = "some version";
 
@@ -37,20 +27,12 @@ describe('VersionInfoStore', () => {
 
         expect(store.hasVersionInfo).toBe(false);
         expect(store.brewpiVersion).toBe("");
-        // add similar checks for other state properties
     });
 
     it('gets version info correctly', async () => {
-        // Mock the mande library and the HTTP response here
-        const mockGet = jest.fn().mockResolvedValue({
-            v: 'version',
-            n: 'revision',
-            c: 'tag',
-            s: 'shield',
-            b: 'board',
-            l: 'log',
-            e: 'ferment',
-        });
+        const fixtureData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'fixtures/api.version.json'), 'utf-8'));
+
+        const mockGet = jest.fn().mockResolvedValue(fixtureData);
         mande.mockImplementation(() => {
             return {
                 get: mockGet,
@@ -61,13 +43,12 @@ describe('VersionInfoStore', () => {
 
         await store.getVersionInfo();
 
-        // Check the state properties after the getVersionInfo action
-        expect(store.brewpiVersion).toBe('version');
-        expect(store.gitRevision).toBe('revision');
-        expect(store.gitTag).toBe('tag');
-        expect(store.brewpiShield).toBe('shield');
-        expect(store.brewpiBoard).toBe('board');
-        expect(store.brewpiLogMessagesVersion).toBe('log');
-        expect(store.fermenttempVersion).toBe('ferment');
+        expect(store.brewpiVersion).toBe(fixtureData.v);
+        expect(store.gitRevision).toBe(fixtureData.n);
+        expect(store.gitTag).toBe(fixtureData.c);
+        expect(store.brewpiShield).toBe(fixtureData.s);
+        expect(store.brewpiBoard).toBe(fixtureData.b);
+        expect(store.brewpiLogMessagesVersion).toBe(fixtureData.l);
+        expect(store.fermenttempVersion).toBe(fixtureData.e);
     });
 });
