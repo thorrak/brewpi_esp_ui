d2992e12d6f21d382cd04b66d36a2cfa61644c2b
John <thorrak@users.noreply.github.com>
2023-07-21 20:09:05 -0400

    Add test for UpstreamSettingsStore.js

--- src/stores/UpstreamSettingsStore.js
+++ src/stores/UpstreamSettingsStore.js
@@ -1,75 +1,74 @@
 import { defineStore } from 'pinia';
 import { mande } from 'mande'
 import { genCSRFOptions } from './CSRF';
+import { ref } from 'vue';
 
-export const useUpstreamSettingsStore = defineStore("UpstreamSettingsStore", {
-    state: () => {
-        return {
-            hasUpstreamSettings: false,
-            upstreamSettingsError: false,
-            upstreamHost: "",
-            upstreamPort: 0,
-            username: "",  // Generally will not be stored beyond registration
-            apiKey: "",
-            upstreamRegistrationError: 7,
-            deviceID: "",
-        };
-    },
-    actions: {
-        async getUpstreamSettings() {
-            try {
-                const remote_api = mande("/api/upstream/", genCSRFOptions());
-                const response = await remote_api.get();
-                if (response) {
-                    this.hasUpstreamSettings = true;
-                    this.upstreamSettingsError = false;
-                    this.upstreamHost = response.upstreamHost;
-                    this.upstreamPort = response.upstreamPort;
-                    this.deviceID = response.deviceID;
-                    this.username = response.username;
-                    this.apiKey = response.apiKey;
-                    this.upstreamRegistrationError = response.upstreamRegistrationError;
+export const useUpstreamSettingsStore = defineStore("UpstreamSettingsStore", () => {
+    const hasUpstreamSettings = ref(false);
+    const upstreamSettingsError = ref(false);
+    const upstreamHost = ref("");
+    const upstreamPort = ref(0);
+    const username = ref("");
+    const apiKey = ref("");
+    const upstreamRegistrationError = ref(7);
+    const deviceID = ref("");
 
-                } else {
-                    await this.clearUpstreamSettings();
-                    this.upstreamSettingsError = true;
-                }
-            } catch (error) {
-                await this.clearUpstreamSettings();
-                this.upstreamSettingsError = true;
+    async function getUpstreamSettings() {
+        try {
+            const remote_api = mande("/api/upstream/", genCSRFOptions());
+            const response = await remote_api.get();
+            if (response) {
+                hasUpstreamSettings.value = true;
+                upstreamSettingsError.value = false;
+                upstreamHost.value = response.upstreamHost;
+                upstreamPort.value = response.upstreamPort;
+                deviceID.value = response.deviceID;
+                username.value = response.username;
+                apiKey.value = response.apiKey;
+                upstreamRegistrationError.value = response.upstreamRegistrationError;
+
+            } else {
+                await clearUpstreamSettings();
+                upstreamSettingsError.value = true;
             }
-        },
-        async clearUpstreamSettings() {
-            this.hasUpstreamSettings = false;
-            this.upstreamHost = "";
-            this.upstreamPort = 0;
-            this.deviceID = "";
-            this.username = "";
-            this.apiKey = "";
-            this.upstreamRegistrationError = 7;
-        },
-        async setUpstreamSettings(upstreamHost, upstreamPort, resetDeviceID, username, apiKey) {
-            try {
-                const remote_api = mande("/api/upstream/", genCSRFOptions());
-                const response = await remote_api.put({
-                    upstreamHost: upstreamHost,  // String
-                    upstreamPort: Number(upstreamPort),  // Integer
-                    resetDeviceID: resetDeviceID, // Boolean
-                    username: username, // String
-                    apiKey: apiKey, // String
-                    // deviceID: deviceID, // Not processed in the firmware currently
-                });
-                if (response && response.message) {
-                    // TODO - Check response.message
-                    await this.getUpstreamSettings()
-                } else {
-                    await this.clearUpstreamSettings();
-                    this.upstreamSettingsError = true;
-                }
-            } catch (error) {
-                await this.clearUpstreamSettings();
-                this.upstreamSettingsError = true;
-            }
+        } catch (error) {
+            await clearUpstreamSettings();
+            upstreamSettingsError.value = true;
         }
-    },
+    }
+
+    async function clearUpstreamSettings() {
+        hasUpstreamSettings.value = false;
+        upstreamHost.value = "";
+        upstreamPort.value = 0;
+        deviceID.value = "";
+        username.value = "";
+        apiKey.value = "";
+        upstreamRegistrationError.value = 7;
+    }
+
+    async function setUpstreamSettings(upstreamHostParam, upstreamPortParam, resetDeviceID, usernameParam, apiKeyParam) {
+        try {
+            const remote_api = mande("/api/upstream/", genCSRFOptions());
+            const response = await remote_api.put({
+                upstreamHost: upstreamHostParam,  // String
+                upstreamPort: Number(upstreamPortParam),  // Integer
+                resetDeviceID: resetDeviceID, // Boolean
+                username: usernameParam, // String
+                apiKey: apiKeyParam, // String
+                // deviceID: deviceID, // Not processed in the firmware currently
+            });
+            if (response && response.message) {
+                await getUpstreamSettings();
+            } else {
+                await clearUpstreamSettings();
+                upstreamSettingsError.value = true;
+            }
+        } catch (error) {
+            await clearUpstreamSettings();
+            upstreamSettingsError.value = true;
+        }
+    }
+
+    return { hasUpstreamSettings, upstreamSettingsError, upstreamHost, upstreamPort, username, apiKey, upstreamRegistrationError, deviceID, getUpstreamSettings, clearUpstreamSettings, setUpstreamSettings };
 });
