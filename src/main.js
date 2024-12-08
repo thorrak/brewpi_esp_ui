b7e1fa6a8a43a7c82c1ed91c012e61b1e3f7a6db
John <thorrak@users.noreply.github.com>
2024-12-08 18:56:06 -0500

    Implement `GetBrowserLocales.js`

--- src/main.js
+++ src/main.js
@@ -1,42 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { LoadingPlugin } from 'vue-loading-overlay';
+import { getBrowserLocales } from 'mixins/GetBrowserLocales'
+
 import router from './router'
 import App from './App.vue'
+
 import './style.css'
 import 'vue-loading-overlay/dist/css/index.css';
+
 
 // import translations
 import es from "./locales/es.json";
 import en from "./locales/en.json";
-
-
-function getBrowserLocales(options = {}) {
-    const defaultOptions = {
-        languageCodeOnly: false,
-    };
-
-    const opt = {
-        ...defaultOptions,
-        ...options,
-    };
-
-    const browserLocales =
-        navigator.languages === undefined
-            ? [navigator.language]
-            : navigator.languages;
-
-    if (!browserLocales) {
-        return undefined;
-    }
-
-    return browserLocales.map(locale => {
-        const trimmedLocale = locale.trim();
-        return opt.languageCodeOnly
-            ? trimmedLocale.split(/-|_/)[0]
-            : trimmedLocale;
-    });
-}
 
 // configure i18n
 import { createI18n } from "vue-i18n";
