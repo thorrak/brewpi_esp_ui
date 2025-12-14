ff574a315f8c7b92b7b5a0b2eb80887a0bef041f
Thorrak <thorrak@excelinspace.com>
2025-12-14 11:09:38 -0500

    Fix bug causing persistent "grey screen" on mode change

--- src/components/dashboard/ModeChangeModal.vue
+++ src/components/dashboard/ModeChangeModal.vue
@@ -120,7 +120,7 @@
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
         <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
           <div class="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-            <div v-if="BrewPiSensorStore.deviceUpdateError">
+            <div v-if="TempControlStore.setModeError">
               <div class="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
                 <NoSymbolIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
               </div>
